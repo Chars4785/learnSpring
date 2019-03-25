e19c942036d780aedc8b01369830ffea0bfeaa8b
Unknown <9105lgm@naver.com>
2019-03-26 01:06:11 +0900

    main

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int temp =sc.nextInt();
+        int[] ary = new int[1000];
+
+
+        for(int i=0;i<temp;i++)
+            System.out.println(ary[i]);
+
+
+
+
+    }
+
+
+}
+
